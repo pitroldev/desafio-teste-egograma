64d4b7a31741f25976b5b2f7029d2e948b2682c6
Petro Cardoso <petrolcds@gmail.com>
2024-08-03 04:30:11 -0300

    :bug: fix encode/decode result

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -1,14 +1,40 @@
+function xorEncrypt(text: string, key: string): string {
+  const textChars = text.split("").map((char) => char.charCodeAt(0));
+  const keyChars = key.split("").map((char) => char.charCodeAt(0));
+  const encryptedChars = textChars.map(
+    (char, i) => char ^ keyChars[i % keyChars.length]
+  );
+  return encryptedChars.map((char) => String.fromCharCode(char)).join("");
+}
+
+function xorDecrypt(text: string, key: string): string {
+  const textChars = text.split("").map((char) => char.charCodeAt(0));
+  const keyChars = key.split("").map((char) => char.charCodeAt(0));
+  const decryptedChars = textChars.map(
+    (char, i) => char ^ keyChars[i % keyChars.length]
+  );
+  return decryptedChars.map((char) => String.fromCharCode(char)).join("");
+}
+
+const secretKey = "532-098poi923itjk";
+
 export function encodeEgogramResult(answers: (number | null)[]): string {
   const jsonString = JSON.stringify(answers);
 
-  return encodeURI(Buffer.from(jsonString).toString("base64"));
+  const encrypted = xorEncrypt(jsonString, secretKey);
+
+  return Buffer.from(encrypted).toString("base64");
 }
 
 export function decodeEgogramResult(encoded: string): (number | null)[] {
   try {
-    const jsonString = Buffer.from(encoded, "base64").toString("utf-8");
+    const encrypted = Buffer.from(
+      decodeURIComponent(encoded),
+      "base64"
+    ).toString("utf-8");
+    const decrypted = xorDecrypt(encrypted, secretKey);
 
-    const parsedArray = JSON.parse(jsonString);
+    const parsedArray = JSON.parse(decrypted);
     if (!Array.isArray(parsedArray)) throw new Error("Invalid array");
 
     return parsedArray;
