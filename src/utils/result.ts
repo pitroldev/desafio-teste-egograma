762f8786c4111860877a2dab16e58b93a753ddcf
Petro Cardoso <petrolcds@gmail.com>
2024-08-03 03:59:44 -0300

    :bug: Ajuste no resultado

--- src/utils/result.ts
+++ src/utils/result.ts
@@ -2,10 +2,14 @@ export function encodeEgogramResult(answers: (number | null)[]): string {
   const strigifiedAnswers = answers.map((a) => a?.toString()) as string[];
   const joinedAnswers = strigifiedAnswers.join("");
 
-  return Buffer.from(joinedAnswers).toString("base64");
+  return encodeURI(Buffer.from(joinedAnswers).toString("base64"));
 }
 
 export function decodeEgogramResult(encoded: string): (number | null)[] {
-  const decoded = Buffer.from(encoded, "base64").toString("utf-8");
-  return decoded.split("").map((a) => (a === "" ? null : parseInt(a)));
+  const decoded = Buffer.from(decodeURI(encoded), "base64").toString();
+
+  return decoded
+    .split("")
+    .map((a) => (a === "" ? null : parseInt(a)))
+    .filter((a) => !isNaN(a ?? NaN));
 }
